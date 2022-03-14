590fe403ae413c85fd89547b68595343258c7f88
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-03-14 23:42:36 +0300

    Создает компонент FilmCard и подключает его в HomePage.

---
+++ project/src/components/film-card/film-card.tsx
@@ -0,0 +1,29 @@
+/**
+ * Карточка фильма.
+ */
+import {FilmCardData} from '../../types/FilmCardData';
+
+function FilmCard({coverImgSrc, title}: FilmCardData): JSX.Element {
+  return (
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img
+          src={coverImgSrc}
+          alt={title}
+          width="280"
+          height="175"
+        />
+      </div>
+      <h3 className="small-film-card__title">
+        <a
+          className="small-film-card__link"
+          href="film-page.html"
+        >
+          {title}
+        </a>
+      </h3>
+    </article>
+  );
+}
+
+export default FilmCard;
---
+++ project/src/components/home-page/films-data.mock.ts
@@ -0,0 +1,88 @@
+import {FilmCardData} from '../../types/FilmCardData';
+
+/**
+ * Выносим моковые данные о фильмах в отдельный файл для повышения читаемости.
+ */
+export const filmsDataMock: readonly FilmCardData[] = [
+  new FilmCardData(
+    'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    'Fantastic Beasts: The Crimes of Grindelwald',
+  ),
+  new FilmCardData(
+    'img/bohemian-rhapsody.jpg',
+    'Bohemian Rhapsody',
+  ),
+  new FilmCardData(
+    'img/macbeth.jpg',
+    'Macbeth',
+  ),
+  new FilmCardData(
+    'img/aviator.jpg',
+    'Aviator',
+  ),
+  new FilmCardData(
+    'img/we-need-to-talk-about-kevin.jpg',
+    'We need to talk about Kevin',
+  ),
+  new FilmCardData(
+    'img/what-we-do-in-the-shadows.jpg',
+    'What We Do in the Shadows',
+  ),
+  new FilmCardData(
+    'img/revenant.jpg',
+    'Revenant',
+  ),
+  new FilmCardData(
+    'img/johnny-english.jpg',
+    'Johnny English',
+  ),
+  new FilmCardData(
+    'img/shutter-island.jpg',
+    'Shutter Island',
+  ),
+  new FilmCardData(
+    'img/pulp-fiction.jpg',
+    'Pulp Fiction',
+  ),
+  new FilmCardData(
+    'img/no-country-for-old-men.jpg',
+    'No Country for Old Men',
+  ),
+  new FilmCardData(
+    'img/snatch.jpg',
+    'Snatch',
+  ),
+  new FilmCardData(
+    'img/moonrise-kingdom.jpg',
+    'Moonrise Kingdom',
+  ),
+  new FilmCardData(
+    'img/seven-years-in-tibet.jpg',
+    'Seven Years in Tibet',
+  ),
+  new FilmCardData(
+    'img/midnight-special.jpg',
+    'Midnight Special',
+  ),
+  new FilmCardData(
+    'img/war-of-the-worlds.jpg',
+    'War of the Worlds',
+  ),
+  new FilmCardData(
+    'img/dardjeeling-limited.jpg',
+    'Dardjeeling Limited',
+  ),
+  new FilmCardData(
+    'img/orlando.jpg',
+    'Orlando',
+  ),
+  new FilmCardData(
+    'img/mindhunter.jpg',
+    'Mindhunter',
+  ),
+  new FilmCardData(
+    'img/midnight-special.jpg',
+    'Midnight Special',
+  ),
+
+];
