5d54461db648cef6ee5147a918dd905582b86fe9
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-03-14 23:58:41 +0300

    Создает компонент PromoFilmCard и подключает его в HomePage.

--- project/src/components/home-page/home-page.tsx
+++ project/src/components/home-page/home-page.tsx
@@ -3,6 +3,7 @@
  */
 import {filmsDataMock} from './films-data.mock';
 import FilmCard from '../film-card/film-card';
+import PromoFilmCard from '../promo-film-card/promo-film-card';
 
 function HomePage(): JSX.Element {
 
@@ -36,41 +37,14 @@ function HomePage(): JSX.Element {
           </ul>
         </header>
 
-        <div className="film-card__wrap">
-          <div className="film-card__info">
-            <div className="film-card__poster">
-              <img
-                src="img/the-grand-budapest-hotel-poster.jpg"
-                alt="The Grand Budapest Hotel poster"
-                width="218"
-                height="327"
-              />
-            </div>
+        {/*Промо фильм*/}
+        <PromoFilmCard
+          title="The Grand Budapest Hotel"
+          genre="Drama"
+          year={2014}
+          posterImgSrc="img/the-grand-budapest-hotel-poster.jpg"
+        />
 
-            <div className="film-card__desc">
-              <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">Drama</span>
-                <span className="film-card__year">2014</span>
-              </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
       </section>
       <div className="page-content">
         <section className="catalog">
